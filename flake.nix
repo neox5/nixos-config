4a8d93e1e021815551d16c3007b678ed79fcd6f3
neox5 <neox5@nixos>
2024-02-08 10:17:11 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  };
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ ./configuration.nix ];
+    };
+  };
+}
